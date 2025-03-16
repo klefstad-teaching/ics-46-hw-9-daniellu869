b02eba03b2cefa30cbc0283f0c779837301272a0
Daniel Lu <dhlu1@uci.edu>
2025-03-16 21:13:54 +0000

    Back up push with one case edit_distance_within

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -4,69 +4,35 @@ void error(string word1, string word2, string msg){
     cout << word1 << ' ' << word2 << ' ' << msg << endl;
 }
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d){
-    // cout << str1 << ' ' << str2 << endl;
-    if (d < 2){ //optimization for adjacent
-        // cout << " d < 2 " << endl;
-        int m = str1.size();
-        int n = str2.size();
-        if (abs(m-n) > 1) return false;
-        int dist = 0;
-        // cout << " distance within range " << endl;
-        if (m-n == 0) {
-            // cout << " equal length for loop " << endl;
-            for (int i = 0; i < m; ++i) {
-                if (str1[i] != str2[i]) ++dist;
-                if (dist > 1) return false;
-            }
-            return true;
+    int m = str1.size();
+    int n = str2.size();
+    if (abs(m-n) > 1) return false;
+    int dist = 0;
+    if (m-n == 0) {
+        for (int i = 0; i < m; ++i) {
+            if (str1[i] != str2[i]) ++dist;
+            if (dist > 1) return false;
         }
-        else if (m-n > 0) {
-            for (int i = 0; i < n; ++i) {
-                if (str1[i + dist] != str2[i]) {
-                    ++dist;
-                    --i;
-                    if (dist > 1) return false;
-                }
-            }
-            return dist < 2;
-        }
-        for (int i = 0; i < n; ++i){
-            if (str1[i] != str2[i + dist]) {
+        return true;
+    }
+    else if (m-n > 0) {
+        for (int i = 0; i < n; ++i) {
+            if (str1[i + dist] != str2[i]) {
                 ++dist;
                 --i;
                 if (dist > 1) return false;
             }
         }
-        return true;
-
+        return dist < 2;
     }
-    int m = str1.size() + 1;
-    int n = str2.size() + 1;
-    // cout << m << ' ' << n << endl;
-    vector<vector<int>> dist(m, vector(n, 0));
- 
-    for (int i = 1; i < m; ++i) dist[i][0] = i;
-    // cout << "row" << endl;
-    for (int j = 1; j < n; ++j) dist[0][j] = j;
-    // cout << "column" << endl;
-
-    for (int j = 1; j < n; ++j){
-        for (int i = 1; i < m; ++i){
-            int substitutionCost = 1;
-            // cout << i << ' ' << j << " before" << endl;
-            if (str1[i-1] == str2[j-1]) substitutionCost = 0;
-            // cout << dist[i][j] << endl;
-            // cout << i << ' ' << j << endl;
-            dist[i][j] = min(min(dist[i-1][j] + 1,                   // deletion
-                                dist[i][j-1] + 1),                 // insertion
-                                dist[i-1][j-1] + substitutionCost); // substitution
+    for (int i = 0; i < n; ++i){
+        if (str1[i] != str2[i + dist]) {
+            ++dist;
+            --i;
+            if (dist > 1) return false;
         }
     }
-    // for (int i = 0; i < m; ++i){
-    //     for (int j = 0; j < n; ++j) cout << dist[i][j] << ' ';
-    //     cout << endl;
-    // }
-    return dist[m-1][n-1] <= d;
+    return true;
 }
 bool is_adjacent(const string& word1, const string& word2){
     // cout << "called" << endl;
